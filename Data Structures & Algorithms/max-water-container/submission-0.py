0bfb678e383dc99d53af43dfc2b2cf80ec19f95b
mypytestmailid-droid <mypytestmailid@gmail.com>
2026-08-19 12:54:07 -0500

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        left , right = 0 , len(heights)-1
+        best = 0
+        while left < right:
+            width = right - left #
+            h = min(heights[left], heights[right])
+            best = max( best, width * h)
+
+            if heights[left] < heights[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return best
